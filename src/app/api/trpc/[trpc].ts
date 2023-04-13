ec48a7c9fd8b32bda5cd778e135b58ca7274961d
Meriegg <ya.boybeats1234@gmail.com>
2023-04-13 16:59:16 +0300

    Added trpc and prisma

---
+++ src/app/api/trpc/[trpc].ts
@@ -0,0 +1,10 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { createTRPCContext } from "@/server/api/trpc";
+import { appRouter } from "@/server/api/root";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createTRPCContext,
+});
