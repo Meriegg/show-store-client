259a2006501e6aa0221317a791596a8a6dbfba4b
Meriegg <ya.boybeats1234@gmail.com>
2023-04-28 13:59:40 +0300

    Added filter functionality to store page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,29 +1,16 @@
 "use client";
 
-import LoadingText from "@/components/LoadingText";
-import Product from "@/components/application/Store/Product";
+import StoreProducts from "@/components/application/Store/StoreProducts";
 import Sidebar, { MobileFilter } from "@/components/application/Store/sidebar";
 import StoreTopBar from "@/components/application/Store/topBar";
-import { trpc } from "@/utils/trpc-provider";
 
 const StorePage = () => {
-  const { isLoading, data, isError, error } = trpc.products.getProducts.useQuery();
-
   return (
     <div className="flex">
       <Sidebar />
       <div className="flex flex-col gap-2 w-full relative">
         <StoreTopBar />
-        {!data?.length && !isLoading && <p>No products were found {":("}</p>}
-        {isLoading && <LoadingText />}
-        <p>{error?.message}</p>
-
-        <div className="flex gap-6 w-full p-4 flex-wrap justify-evenly">
-          {!isLoading &&
-            !isError &&
-            data.map((product, idx) => <Product storeProduct={product} key={idx} />)}
-        </div>
-
+        <StoreProducts />
         <MobileFilter />
       </div>
     </div>
